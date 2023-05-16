65a52bc22d59f5fb6fb17c4d5e76065b117e3438
Catarina Martins Diniz <catarina.cmd@gmail.com>
2023-05-16 12:16:04 -0300

    aba 'inicio' removida
    removi a aba 'inicio' do menu e inseri o gráfico na aba 'meu conteúdo'
    pretendo inserir a funcionalidade de gerenciamento do perfil no último botão da aba, onde terá um ícone que, ao clicar, irá abrir um menu onde o usuário poderá deslogar ou gerenciar o perfil (mudando email e senha)

--- src/components/AbaMenu.js
+++ src/components/AbaMenu.js
@@ -1,35 +1,25 @@
 import React, { useState } from 'react';
 import ToDoList from './ToDoList';
-import InsightChart from './InsightChart';
 import { AiOutlineLogin } from 'react-icons/ai';
 
 
 function AbaMenu(){
 
-    const [chart, setChart] = useState();
     const [toDoList, setToDoList] = useState();
     const [cron, setCron] = useState();
-    const [tarefas] = useState([]);
 
 
-function HandleClickChart(){
-    setToDoList(false);
-    setCron(false);
-    setChart(!chart);
-
-}
 
 function HandleClickToDoList(){
     setToDoList(!toDoList);
     setCron(false);
-    setChart(false);
+
 
 }
 
 function HandleClickCron(){
     setToDoList(false);
     setCron(!cron);
-    setChart(false);
 
 }
 
@@ -37,7 +27,6 @@ return (
     <div>
         <nav className="menu">
             <ul>
-              <li><a href="#inicio" onClick={HandleClickChart}>inicio</a></li>
               <li><a href="#meuConteudo" onClick={HandleClickToDoList}>meu conteudo</a></li>
               <li><a href="#cronograma" onClick={HandleClickCron}>cronograma</a></li>
               <li><a href="#login"><AiOutlineLogin/></a></li>
@@ -45,9 +34,6 @@ return (
         </nav>
 
         <div>
-            {chart === true && <InsightChart tarefas={tarefas} />}
-            {!chart && <div></div>}
-
             <div>
             {toDoList === true && <ToDoList />}
             {!toDoList && <div></div>}
--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import '../App.css';
 
+/* INICIO GERENCIADOR DE TAREFAS */
+
 function ToDoForm(props) {
   const [tarefa, defineTarefa] = useState('');
   const [dificuldade, setDificuldade] = useState('');
@@ -38,63 +40,84 @@ function ToDoForm(props) {
     setDificuldade('');
     setHorasDisponiveis(0); 
   }
+  /* FIM GERENCIADOR DE TAREFAS */
 
+
+  /* INICIO GRÁFICO DE TAREFAS */
+  /*function grafico() {}*/
+  /* FIM GRÁFICO DE TAREFAS */
 
   return (
     <div className="tarefas">
-      <form onSubmit={inserirTarefa}>
-        <div>
-          <input 
-          type="text"
-          className="form" 
-          placeholder="Digite uma tarefa..." 
-          value={tarefa} 
-          onChange={criarTarefa} />
+         {/* INICIO ELEMENTO 1 = TAREFAS */}
+         <div className='input-lista'>
+            <form onSubmit={inserirTarefa}>
+                <div className='input'>
+                  
+                  <input 
+                  type="text"
+                  className="form" 
+                  placeholder="Digite uma tarefa..." 
+                  value={tarefa} 
+                  onChange={criarTarefa} />
 
-          <select 
-          className="diff" value={dificuldade} onChange={selecionarDificuldade}>
-            <option value="">Selecione a dificuldade</option>
-            <option value="facil">Fácil</option>
-            <option value="medio">Médio</option>
-            <option value="dificil">Difícil</option>
-          </select>
-          <input className="hours" type="number" value={horasDisponiveis} onChange={selecionarHorasDisponiveis} />
-          <button className="adicionarTarefa" type="submit">Adicionar</button>
-        </div>
+                  <select 
+                    className="diff" value={dificuldade} onChange={selecionarDificuldade}>
+                    <option value="">Selecione a dificuldade</option>
+                    <option value="facil">Fácil</option>
+                    <option value="medio">Médio</option>
+                    <option value="dificil">Difícil</option>
+                  </select>
 
-      </form>
-      
-      <div className= "listaTarefas">
-        <table>
-          <thead>
-            <tr>
-              <th>Tarefas</th>
-              <th>Dificuldade</th>
-              <th>Horas disponíveis</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tarefas.map((tarefa, index) => (
-              <tr key={index}>
-                <td>{tarefa.tarefa}</td>
-                <td>{tarefa.dificuldade}</td>
-                <td>{tarefa.horasDisponiveis}</td>
-                <td>
-                  <label className="custom-checkbox">
-                    <input type="checkbox" checked={tarefa.concluida} onChange={() => toggle(index)} />
-                  </label>
-                </td>
-              </tr>
-            ))
-            }
-          </tbody>
-        </table>
-      </div>
+                  <input 
+                  className="hours" 
+                  type="number" 
+                  value={horasDisponiveis} 
+                  onChange={selecionarHorasDisponiveis} />
 
+                  <button className="adicionarTarefa" type="submit">Adicionar</button>
 
-            
-    </div>
+                </div>
+            </form>
+        
+                <div className= "listaTarefas">
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Tarefas</th>
+                          <th>Dificuldade</th>
+                          <th>Horas disponíveis</th>
+                          <th>Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {tarefas.map((tarefa, index) => (
+                          <tr key={index}>
+                            <td>{tarefa.tarefa}</td>
+                            <td>{tarefa.dificuldade}</td>
+                            <td>{tarefa.horasDisponiveis}</td>
+                            <td>
+                              <label className="custom-checkbox">
+                                <input type="checkbox" checked={tarefa.concluida} onChange={() => toggle(index)} />
+                              </label>
+                            </td>
+                          </tr>
+                        ))
+                        }
+                      </tbody>
+                    </table>
+                  </div>
+            </div>
+          {/* FIM ELEMENTO 1 = TAREFAS */}
+
+            <div className='grafico'>
+          {/* INICIO ELEMENTO 2 = GRAFICO */}
+          
+
+           </div>
+
+        </div> 
+
   );
 }
 
