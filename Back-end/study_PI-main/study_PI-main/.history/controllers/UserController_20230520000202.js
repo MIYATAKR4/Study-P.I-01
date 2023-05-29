ec464db0438005e963ef342cbd65c66fea09989e
Catarina <62525104+MIYATAKR4@users.noreply.github.com>
2023-05-29 18:44:42 -0300

    Delete Back-end/study_PI-main directory

--- Back-end/study_PI-main/study_PI-main/.history/controllers/UserController_20230520000202.js
+++
@@ -1,7 +0,0 @@
-const user = require('../models/User')
-
-module.exports = class UserController {
-    static async register (res, req) {
-        res.json("oi")
-    }
-}
--- Back-end/study_PI-main/study_PI-main/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-async function main(){
-    await mongoose.connect('mongodb://localhost:27017')
-    console.log('Conectou ao banco Mongoose')
-}
-main().catch((err) => console.log(err))
-
-module.exports = mongoose
